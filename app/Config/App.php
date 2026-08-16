d376c7bb157aee5e4dcf4807970f8b8464d6209b
zusfan mashuri <55775824+zusfan-ops@users.noreply.github.com>
2026-08-16 12:48:32 +0700

    fix: configure allowedHostnames and correct baseURL for live domain

--- app/Config/App.php
+++ app/Config/App.php
@@ -29,7 +29,7 @@ class App extends BaseConfig
      *
      * @var list<string>
      */
-    public array $allowedHostnames = [];
+    public array $allowedHostnames = ['duitku.ordr.my.id', 'duitku.test', 'localhost'];
 
     /**
      * --------------------------------------------------------------------------
